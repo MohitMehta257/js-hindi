8b378b5a73f20afd6fefa5f164bbe76fb3132291
MohitMehta257 <58682658+MohitMehta257@users.noreply.github.com>
2025-03-19 21:35:00 +0000

    Datatypes Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// Primitive - Call By Value
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Non Primitive - Reference
+
+// Arrays, Object, Functions
+
+const score=100
+// JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime.
+// Unlike languages such as Typescript or Java, you don’t need to declare the data type of a variable explicitly.
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Memory
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+// In Stack Memory we get the copy of the value
+// In Heap Memory we get the reference of the value
