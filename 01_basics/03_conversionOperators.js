c2c3ea341cd53631a01d4030d2cf181c8d33fd6a
MohitMehta257 <58682658+MohitMehta257@users.noreply.github.com>
2025-03-19 20:27:16 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,22 @@
+let score="33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+
+console.log(valueInNumber)
+
+
+// "33"=> 33
+// "33abc"=>NaN
+// true=> 1; false=>0
+ let isLoggedIn="mohit"
+ let booleanLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanLoggedIn)
+
+ let someNumber=33
+
+ let stringNUmber=String(someNumber)
+ console.log(typeof stringNUmber)
+
